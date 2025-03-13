2bd1e385b17c9008726d702977521f3a63dea818
Firmin HEUZE <firmin.heuze@gmail.com>
2025-03-13 10:12:15 +0100

    Clean ControllerSnakeGame and move to ContorllerClient

--- src/Client/Controller/ControllerClient.java
+++ src/Client/Controller/ControllerClient.java
@@ -1,1 +1,30 @@
 package Client.Controller;
+
+import Client.View.ViewSnakeGame;
+import Client.View.PanelSnakeGame;
+import Server.Model.InputMap;
+
+class ControllerClient {
+	ViewSnakeGame viewSnakeGame;
+
+	public ControllerClient() {
+		var im = inputMap("/layouts/arenaNoWall.lay");
+
+		var p = new PanelSnakeGame(
+			im.getSizeX(), im.getSizeY(),
+			im.get_walls(),
+			im.getStart_snakes(), im.getStart_items()
+		);
+		viewSnakeGame = new ViewSnakeGame(p);
+	}
+
+	public InputMap inputMap(String filename) {
+		try {
+			return new InputMap(filename);
+		} catch (Exception e) {
+			System.out.println("Couldn't import layout file");
+			System.exit(69);
+		}
+		return null;
+	}
+}
