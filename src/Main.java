731ca45adcb1441694d5676fa29f9c473c371a9a
leg7 <dev@leonardgomez.xyz>
2025-03-13 09:59:43 +0100

    Add arg handling in Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
+	static int ERROR_BAD_ARG = 1;
+
+	public static void badArg() {
+		System.err.println("Please run with \"server\" or \"client\" argument :)");
+		System.exit(ERROR_BAD_ARG);
+	}
+
 	public static void main(String[] args) {
-		if (args[0].equals("server")) {
-
-		} else {
-
+		if (args.length != 1) {
+			badArg();
 		}
 
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		if (args[0].equals("server")) {
+
+		} else if (args[0].equals("client")) {
+
+		} else {
+			badArg();
+		}
 	}
 }
