c26104315ebc017f0d15787746707ebd67860501
Firmin HEUZE <firmin.heuze@gmail.com>
2025-03-14 10:21:14 +0100

    Add server listening in Client Controller

--- app/src/main/java/Client/Controller/ControllerClient.java
+++ app/src/main/java/Client/Controller/ControllerClient.java
@@ -4,18 +4,52 @@ import Client.View.ViewSnakeGame;
 import Client.View.PanelSnakeGame;
 import Server.Model.InputMap;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.net.Socket;
+
 class ControllerClient {
-	ViewSnakeGame viewSnakeGame;
+	private ViewSnakeGame viewSnakeGame;
 
 	public ControllerClient() {
 		var im = inputMap("/layouts/arenaNoWall.lay");
 
 		var p = new PanelSnakeGame(
-			im.getSizeX(), im.getSizeY(),
-			im.get_walls(),
-			im.getStart_snakes(), im.getStart_items()
-		);
+				im.getSizeX(), im.getSizeY(),
+				im.get_walls(),
+				im.getStart_snakes(), im.getStart_items());
 		viewSnakeGame = new ViewSnakeGame(p);
+	}
+
+	public void start(String serverIP, int port) throws IOException {
+		Socket so = new Socket(serverIP, port);
+
+		BufferedReader soIn = new BufferedReader(new InputStreamReader(so.getInputStream()));
+		PrintWriter soOut = new PrintWriter(so.getOutputStream(), true);
+
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					String msg;
+					while ((msg = soIn.readLine()) != null && !Thread.interrupted()) {
+						System.out.println(msg);
+					}
+				} catch (IOException e) {
+				}
+			}
+		});
+		t.start();
+
+		// while (/* View alive */) {
+		//
+		// }
+
+		t.interrupt();
+
+		so.close();
 	}
 
 	public InputMap inputMap(String filename) {
